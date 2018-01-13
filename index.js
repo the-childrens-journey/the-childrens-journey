6257878af8a61ae92ecb32afe112f6b1ea78cb9d
Ashton Harris <ashton.harris@ey.com>
2018-01-13 14:22:25 -0500

    Updates

--- index.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function(){
-  $('.menu-button').click(function() {
-    $(this).toggleClass('expanded');
-    $('[role="navigation"]').slideToggle('fast', function() {});
-  });
-});
